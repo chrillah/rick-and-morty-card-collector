60005686eb547a76372d3caf046d023061116b9d
chrillah <ch.ronnberg@gmail.com>
2022-11-03 23:41:11 +0100

    name changes

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const toggleButton = document.getElementsByClassName('toggle-btn')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click',()=>{
