3f1158381b9c02b559fec955aa411fbd2300a903
chrillah <ch.ronnberg@gmail.com>
2022-11-08 14:42:07 +0100

    found my api

--- rickandmorty.js
+++ rickandmorty.js
@@ -1,6 +1,43 @@
 // hämtar alla avsnitt
-fetch('https://rickandmortyapi.com/api/episode')
-.then(response=>response.json())
-.then(data=>{
-    console.log(data.results[2])
+
+const url = 'https://rickandmortyapi.com/api'
+
+// SKA HÄMTA ALLA EPISODER MEN GÖR INTE DET....
+// fetch(url+'/episode')
+// .then(response=>response.json())
+// .then(data=>{
+//     let array = []
+//     console.log(data.results[2])
+
+//     // VARFÖR INTE SKRIVA UT ALLA I EN LISTA???
+//     for (let i = 0; i < data.length; i++){
+//         //array.push(data[i])
+//         console.log(data)
+//     }
+
+//     console.log(array)
+// })
+
+//episode object
+// fetch(url+'/episode/3')
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+// })
+
+
+
+
+const displayTest = document.querySelector('#display-test')
+const img = document.createElement('img')
+// karaktärer
+fetch(url+'/character/1')
+.then(response => response.json())
+.then(data => {
+    img.src = data.image
+    console.log(data)
+    console.log(data.name)
+    console.log(data)
 })
+
+displayTest.appendChild(img)
